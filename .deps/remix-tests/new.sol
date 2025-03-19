1096a762f6245053370ce42b5c8589f30a3ae255
code-razz <rajsah5557+git@gmail.com>
2025-03-19 13:03:26 +0530

    Lab 2 completed

--- .deps/remix-tests/new.sol
+++ .deps/remix-tests/new.sol
@@ -27,33 +27,38 @@ contract conditional{
 
     //Lab works
     // Task 1: Implement the isPositive function
-    // function isPositive(int256 num) public pure{
-    //     if(num < 0){
-    //         console.log(num , " is negative");
-    //     }else{
-    //         console.log(num , " is positive");
-    //     }
-    // }
-
-        // // Task 1: Implement the isPositive function
-        // function isPositive(int256 num) public pure returns(bool){
-        //     if(num < 0){
-        //         return (num<0);
-        //     }else{
-        //         return (num>0);
-        //     }
-        // }
+    function isPositive(int256 num) public pure{
+        if(num < 0){
+            console.log(" is negative");
+        }else{
+            console.log(" is positive");
+        }
+    }
 
     // Task 2: Implement the recursiveSum function using a while loop
     function recSum(uint num) public pure returns(uint){
         uint sum;
-        while(num>0){
-            sum =sum + num;
-            num--;
+        uint i=0;
+        while(i<=num){
+            sum =sum + i;
+            i++;
         }
         console.log(sum);
         return sum;
         // emit LogInput("hello",sum);
+    }
+
+    //Task 3: Implement the sumBackwards function using a do-while loop
+    function sumBackwards(uint num) public pure returns(uint){
+        uint sum;
+        do{
+            sum =sum + num;
+            num--;
+        }while(num>0);
+        console.log(sum);
+        return sum;
+        // emit LogInput("hello",sum);
+
     }
 
     //Task 4: Implement the factorialFuntion using a for loop
@@ -64,9 +69,32 @@ contract conditional{
         }
         console.log("Factorial of ", num ," is ", ans);
         return ans;
-
     }
 
+    //Task 5: Implement the stopCountAtFive function using the break statement
+    function stopAt5(uint num) public pure returns(uint){
+        while(true){
+            if(num<= 5){
+                break;
+            }else{
+                console.log(num);
+            }
+            num--;
+        }
+        return num;
+    }
 
+    // Task 6: Implement the skipSumAtThree function using the continue statement
+    function sumExcept3(uint num) public pure returns(uint){
+        uint sum=0;
+        for(uint i=0;i<=num;i++){
+            if(i==3){
+                continue;
+            }
+            sum = sum + i;
+        }
+        console.log(sum);
+        return sum;
+    }
 
 }
