0d4cfcc4b98af542080c9dc743bc9de410e12d56
code-razz <rajsah5557+git@gmail.com>
2025-03-19 12:32:12 +0530

    2nd commit

--- .deps/remix-tests/new.sol
+++ .deps/remix-tests/new.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.26;
 import "hardhat/console.sol";
 
 contract conditional{
+    // event LogInput(string message, uint number); // Event for logging
     uint x=0;
 
     function incre() public{
@@ -15,4 +16,57 @@ contract conditional{
 
         }
     }
+
+    function Natural() public {
+        while(x<5){
+            console.log(x);
+            x++;
+        }
+        x=0;
+    }
+
+    //Lab works
+    // Task 1: Implement the isPositive function
+    // function isPositive(int256 num) public pure{
+    //     if(num < 0){
+    //         console.log(num , " is negative");
+    //     }else{
+    //         console.log(num , " is positive");
+    //     }
+    // }
+
+        // // Task 1: Implement the isPositive function
+        // function isPositive(int256 num) public pure returns(bool){
+        //     if(num < 0){
+        //         return (num<0);
+        //     }else{
+        //         return (num>0);
+        //     }
+        // }
+
+    // Task 2: Implement the recursiveSum function using a while loop
+    function recSum(uint num) public pure returns(uint){
+        uint sum;
+        while(num>0){
+            sum =sum + num;
+            num--;
+        }
+        console.log(sum);
+        return sum;
+        // emit LogInput("hello",sum);
+    }
+
+    //Task 4: Implement the factorialFuntion using a for loop
+    function fact(uint num) public pure returns(uint){
+        uint ans=1;
+        for(uint i=1;i<=num;i++){
+            ans=ans*i;
+        }
+        console.log("Factorial of ", num ," is ", ans);
+        return ans;
+
+    }
+
+
+
 }
